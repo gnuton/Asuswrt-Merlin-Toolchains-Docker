bb28e9b06a879616ce0dfee8cf5df2d02b85e24b
Antonio Aloisio <gnuton@gnuton.org>
2024-05-18 16:01:19 +0200

    feat: added HND 5.04 be toolchain support

---
+++ envs/bcm-hnd-ax-4.19be.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo ln -sf ~/am-toolchains/brcm-arm-hnd /opt/toolchains
+
+export LD_LIBRARY_PATH=$LD_LIBRARY:/opt/toolchains/crosstools-arm-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1/usr/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY:/opt/toolchains/crosstools-arm_softfp-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1/usr/lib
+export TOOLCHAIN_BASE=/opt/toolchains
+export PATH=$PATH:/opt/toolchains/crosstools-aarch64-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1/usr/bin
+export PATH=$PATH:/opt/toolchains/crosstools-arm-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1/usr/bin
+#export PATH=$PATH:/opt/toolchains/crosstools-arm_softfp-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1/usr/bin
+
